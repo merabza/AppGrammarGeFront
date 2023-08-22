1baca3db227cee2e9f56746b2b43b6950b972009
merabza <merabza@gmail.com>
2023-08-22 12:54:34 +0400

    202308221254

--- appgrammargefrontend/src/AppParameters.ts
+++ appgrammargefrontend/src/AppParameters.ts
@@ -3,11 +3,11 @@
 import { IAppParametersState } from "./appcarcass/redux/slices/appParametersSlice";
 
 export const appParameters: IAppParametersState = {
-  appName: "ენის მოდელის რედაქტორი",
-  baseUrl:
-    process.env.NODE_ENV === "development"
-      ? //? window.location.origin + '/api'
-        // ? 'http://localhost:5022/api/v1'
-        "https://devapp.grammar.ge/api/v1"
-      : "https://app.grammar.ge/api/v1",
+  appName: process.env.REACT_APP_NAME as string, //"ენის მოდელის რედაქტორი",
+  baseUrl: process.env.REACT_APP_API_URI as string,
+  // process.env.NODE_ENV === "development"
+  //   ? //? window.location.origin + '/api'
+  //     // ? 'http://localhost:5022/api/v1'
+  //     "https://devapp.grammar.ge/api/v1"
+  //   : "https://app.grammar.ge/api/v1",
 };
