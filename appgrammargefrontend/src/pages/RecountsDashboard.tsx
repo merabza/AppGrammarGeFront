61ab22a421ba5eec23c06610463c872ee0e80cc7
merabza <merabza@gmail.com>
2026-09-10 21:06:05 +0400

    202609102105

--- appgrammargefrontend/src/pages/RecountsDashboard.tsx
+++ appgrammargefrontend/src/pages/RecountsDashboard.tsx
@@ -182,8 +182,8 @@ const RecountsDashboard: FC = () => {
                                     StrData.ChangedBase === ""
                                 )
                                     setChangedBase(StrData.ChangedBase);
-                                if (StrData.ErrorOmd || StrData.ErrorOmd === "")
-                                    setErrorMessage(StrData.ErrorOmd);
+                                if (StrData.Error || StrData.Error === "")
+                                    setErrorMessage(StrData.Error);
                             }
                             //setLastMessage(receivedMessage);
                         }
@@ -207,7 +207,7 @@ const RecountsDashboard: FC = () => {
                     );
 
                     console.log(
-                        "ErrorOmd while establishing connection: " + { err }
+                        "Error while establishing connection: " + { err }
                     );
                 }
                 setHubConnection(hubConnect);
